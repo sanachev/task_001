3392a87e707f030fdc56455b4e8c984285413fc1
harrioho <stas@example.com>
2024-02-28 00:51:25 +0300

    develop Структура и инпут

--- src/roman_to_arabic.c
+++ src/roman_to_arabic.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#define LENGTH 15
 
-int main(void){
+void input(char *data);
+void calculate(char *data, int *result);
+void output(int result);
 
+int main(void) {
+    char data[LENGTH + 1] = {0};
+    int result = 0;
 
-    printf("Be careful");
+    input(data);
+
+    calculate(data, result);
+
+    output(result);
+
     return 0;
+}
+
+void input(char *data) { scanf("%s", data); }
+
+void calculate(char *data, int *result) { /* some code */
+}
+
+void output(int result) {
+    if (result == 0) {
+        printf("Be careful, Verter");
+    } else {
+        printf("%d", result);
+    }
 }
